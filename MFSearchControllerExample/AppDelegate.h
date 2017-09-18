6b4aaeabac864d5571404566132da5ff3fc715e0
xumengfan <xmf@xkeshi.com>
2017-09-18 13:27:33 +0800

    Initial Commit

---
+++ MFSearchControllerExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MFSearchControllerExample
+//
+//  Created by XuMengFan on 2017/9/18.
+//  Copyright © 2017年 MF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MFSearchControllerExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MFSearchControllerExample
+//
+//  Created by XuMengFan on 2017/9/18.
+//  Copyright © 2017年 MF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
